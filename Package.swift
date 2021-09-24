0f36ea27bbb4059a6122bc44f83f985b9a910228
Stephen Kockentiedt <stephen@kockentiedt.name>
2021-09-24 21:26:00 +0200

    Try to fix dependency error under Swift 5.4
    Error "product dependency 'SwiftSyntax' in package 'swift-syntax' not
    found" should hopefully be fixed by this change.

--- Package.swift
+++ Package.swift
@@ -13,12 +13,12 @@ let package = Package(
             targets: ["SwiftyBridges"]),
         .executable(
             name: "BridgeBuilder",
-            targets: ["BridgeBuilder"])
+            targets: ["BridgeBuilder"]),
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50500.0")),
+        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50500.0")),
         .package(url: "https://github.com/stencilproject/Stencil.git", .upToNextMinor(from: "0.14.1")),
         .package(url: "https://github.com/SwiftGen/StencilSwiftKit", from: "2.8.0"),
     ],
@@ -34,7 +34,7 @@ let package = Package(
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "Stencil", package: "Stencil"),
-                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftSyntax", package: "SwiftSyntax"),
                 .product(name: "StencilSwiftKit", package: "StencilSwiftKit"),
             ],
             resources: [
