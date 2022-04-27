5cd2c96bdb93e1a6ec0661ab78bbbfc7200e8812
Stephen Kockentiedt <stephen@kockentiedt.name>
2022-04-27 13:27:49 +0200

    Rename generated files

--- Plugins/APICodeGenerator/plugin.swift
+++ Plugins/APICodeGenerator/plugin.swift
@@ -5,8 +5,8 @@ import PackagePlugin
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
         let builder = try context.tool(named: "BridgeBuilder")
         let outputDirectory = context.pluginWorkDirectory.appending("GeneratedSources")
-        let serverOutputFile = outputDirectory.appending("server.swift")
-        let clientOutputFile = outputDirectory.appending("client.swift")
+        let serverOutputFile = outputDirectory.appending("GeneratedServerCode.swift")
+        let clientOutputFile = outputDirectory.appending("GeneratedAPIClientCode.swift")
         
         let target = target as! SourceModuleTarget
 
