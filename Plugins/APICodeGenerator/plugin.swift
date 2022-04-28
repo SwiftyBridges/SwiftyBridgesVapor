3e29bc95b90321858124e72618516f2c58f418c9
Stephen Kockentiedt <stephen@kockentiedt.name>
2022-04-28 13:24:20 +0200

    Improve organization of generated files

--- Plugins/APICodeGenerator/plugin.swift
+++ Plugins/APICodeGenerator/plugin.swift
@@ -4,9 +4,13 @@ import PackagePlugin
 @main struct APICodeGenerator: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
         let builder = try context.tool(named: "BridgeBuilder")
-        let outputDirectory = context.pluginWorkDirectory.appending("GeneratedSources")
-        let serverOutputFile = outputDirectory.appending("GeneratedServerCode.swift")
-        let clientOutputFile = outputDirectory.appending("GeneratedAPIClientCode.swift")
+        let workDirectory = context.pluginWorkDirectory
+        let serverOutputFile = workDirectory
+            .appending("GeneratedServerCode")
+            .appending("GeneratedServerCode.swift")
+        let clientOutputFile = workDirectory
+            .appending("GeneratedClientCode")
+            .appending("GeneratedAPIClientCode.swift")
         
         let target = target as! SourceModuleTarget
 
